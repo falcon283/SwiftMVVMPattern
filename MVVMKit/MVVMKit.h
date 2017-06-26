50f532c19392f21ef6081c065f6ad33f35921dc9
Gabriele Trabucco <gabrynet83@gmail.com>
2017-06-26 20:53:00 +0200

    Initial Commit

---
+++ MVVMKit/MVVMKit.h
@@ -0,0 +1,19 @@
+//
+//  MVVMKit.h
+//  MVVMKit
+//
+//  Created by FaLcON2 on 26/06/2017.
+//  Copyright © 2017 Gabriele Trabucco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MVVMKit.
+FOUNDATION_EXPORT double MVVMKitVersionNumber;
+
+//! Project version string for MVVMKit.
+FOUNDATION_EXPORT const unsigned char MVVMKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MVVMKit/PublicHeader.h>
+
+
